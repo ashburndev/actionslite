4f4536915469aa790df05f9f1861de365a7ab464
ashburndev <ashburndev@gmail.com>
2024-07-14 18:43:34 -0400

    initial commit

---
+++ src/main/java/ashburncode/actionslite/ActionsliteApplication.java
@@ -0,0 +1,13 @@
+package ashburncode.actionslite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ashburncode/actionslite/ActionsliteApplicationTests.java
@@ -0,0 +1,13 @@
+package ashburncode.actionslite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
